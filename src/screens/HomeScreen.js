ee80a7401a3f0a76b0a35754d053b95bbcaca97f
Harshal <harshalgangurde@Harshals-MacBook-Pro.local>
2019-02-24 19:57:21 -0800

    use FlatList on home screen. get rid of tiles and tilelist.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,44 @@
 import React, {Component} from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, TouchableHighlight, FlatList, StyleSheet } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
-import TileList from '../components/TileList';
+import DataService from '../services/DataService';
+import NavigationService from '../services/NavigationService';
+
+const SpaceXData = [
+  { id: '1', title: 'missions', screen: 'Missions'},
+  { id: '2', title: 'rockets', screen: 'Rockets'}
+];
 
 export default class HomeScreen extends Component {
 
+  handlePress = (item) => {
+    DataService.getDataForField(item.title)
+      .then((details) => {
+        NavigationService.navigate(item.screen, { data: details })
+      });
+  }
+
   render() {
     return (
-      <View style={ styles.homeScreenView }>
-        <TileList></TileList>
-      </View>
+      <FlatList 
+        data={ SpaceXData }
+        keyExtractor={ (item) => item.id }
+        renderItem={ ({ item }) => (
+          <ListItem 
+            Component={ TouchableHighlight }
+            title={ item.title }
+            titleStyle={{ 
+              textTransform: 'uppercase',
+              fontWeight: 'bold'
+            }}
+            bottomDivider={ true }
+            chevron={ true }
+            onPress={ () => this.handlePress(item) }        
+          />
+        )}
+      />
+
     );
   }
 }
