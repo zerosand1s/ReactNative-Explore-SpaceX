af130f00c8cbf501e800a3e395bddb199deffaf6
Harshal <harshalgangurde@Harshals-MacBook-Pro.local>
2019-03-09 15:15:28 -0800

    add styled view component

---
+++ src/components/StyledView.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import styled from "styled-components/native";
+
+const ViewWrapper = styled.View`
+  flex: ${props => props.flex ? props.flex : 1};
+  padding: ${props => props.padding};
+  backgroundColor: ${props => props.backgroundColor};
+`;
+
+const StyledView = (props) => (
+  <ViewWrapper {...props} />
+);
+
+export default StyledView;
--- src/screens/MissionsScreen.js
+++ src/screens/MissionsScreen.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import { View, Text, FlatList, Modal, StyleSheet, Linking } from 'react-native';
-import { ListItem, Icon } from 'react-native-elements';
+import React, { Component } from "react";
+import { View, Text, FlatList, Modal, StyleSheet, Linking } from "react-native";
+import { ListItem, Icon } from "react-native-elements";
 
-import DataService from '../services/DataService';
+import StyledView from "../components/StyledView";
 
-import globalStyles from '../styles';
+import DataService from "../services/DataService";
+
+import globalStyles from "../styles";
 
 export default class MissionsScreen extends Component {
-
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       loading: true,
       missions: [],
       mission: null,
@@ -19,86 +20,115 @@ export default class MissionsScreen extends Component {
   }
 
   componentDidMount() {
-    DataService.getDataForField('missions')
-      .then((details) => {
-        this.setState({ missions: details, loading: false });
-      });
-  }
-
-  handlePress = (mission) => {
-    this.setState({
-      mission, isModalVisible: true
+    DataService.getDataForField("missions").then(details => {
+      this.setState({ missions: details, loading: false });
     });
   }
 
-  _renderItem = (item) => {
+  handlePress = mission => {
+    this.setState({
+      mission,
+      isModalVisible: true
+    });
+  };
+
+  _renderItem = item => {
     return (
-      <ListItem 
-        title={ item.mission_name }
+      <ListItem
+        title={item.mission_name}
         subtitle={
           <View>
-            <Text style={ styles.missionManufacture }>
-              Manufactured By: { item.manufacturers.length > 1  ? 
-                item.manufacturers[0] + ' and ' + (item.manufacturers.length - 1) + ' more' :
-                item.manufacturers[0] }
+            <Text style={styles.missionManufacture}>
+              Manufactured By:{" "}
+              {item.manufacturers.length > 1
+                ? item.manufacturers[0] +
+                  " and " +
+                  (item.manufacturers.length - 1) +
+                  " more"
+                : item.manufacturers[0]}
             </Text>
-            <Text style={ styles.missionPayload }>Total Payloads: { item.payload_ids.length }</Text>
+            <Text style={styles.missionPayload}>
+              Total Payloads: {item.payload_ids.length}
+            </Text>
           </View>
         }
-        bottomDivider={ true }
-        onPress={ () => this.handlePress(item) }
+        bottomDivider={true}
+        onPress={() => this.handlePress(item)}
       />
     );
-  }
+  };
 
   closeModal = () => {
     this.setState({ mission: null, isModalVisible: false });
-  }
+  };
 
-  _renderModal = (mission) => {
-    return (    
-      <Modal 
-        visible={ this.state.isModalVisible }         
-        animationType={ 'fade' }>
-          <View style={ globalStyles.modalContainer }>
-            <View style={ globalStyles.modalBase }>
-              <View style={ globalStyles.modalCloseIconView }>
-                <Icon 
-                  name='times-circle' 
-                  type='font-awesome' 
-                  size={ 22 }
-                  onPress={ () => this.closeModal() }                         
-                ></Icon>            
-              </View>
-              <View>
-                <Text style={ globalStyles.modalTitle }>{ mission.mission_name }</Text>
-                <Text style={ styles.modalManufacturers }>Manufactured By: { mission.manufacturers.join(', ') }</Text>        
-                <Text style={ styles.modalDescription }>{ mission.description }</Text>
-                <View style={ globalStyles.modalLinkIconsView }>
-                  <Icon name='wikipedia-w' type='font-awesome' size={ 14 } underlayColor='transparent' 
-                    containerStyle={ globalStyles.wikiIcon } onPress={ () => Linking.openURL(mission.wikipedia)}></Icon>
-                  <Icon name='link' type='font-awesome' size={ 14 }  underlayColor='transparent'
-                    containerStyle={ globalStyles.linkIcon } onPress={ () => Linking.openURL(mission.website)}></Icon>
-                  <Icon name='twitter' type='font-awesome' size={ 14 } underlayColor='transparent' 
-                    color={ '#ffffff' } containerStyle={ globalStyles.twitterIcon } onPress={ () => Linking.openURL(mission.twitter)}></Icon>
-                </View>
+  _renderModal = mission => {
+    return (
+      <Modal visible={this.state.isModalVisible} animationType={'fade'}>
+        <StyledView padding={'5%'} backgroundColor={'orange'}>
+          <View style={globalStyles.modalBase}>
+            <View style={globalStyles.modalCloseIconView}>
+              <Icon
+                name="times-circle"
+                type="font-awesome"
+                size={22}
+                onPress={() => this.closeModal()}
+              />
+            </View>
+            <View>
+              <Text style={globalStyles.modalTitle}>
+                {mission.mission_name}
+              </Text>
+              <Text style={styles.modalManufacturers}>
+                Manufactured By: {mission.manufacturers.join(", ")}
+              </Text>
+              <Text style={styles.modalDescription}>{mission.description}</Text>
+              <View style={globalStyles.modalLinkIconsView}>
+                <Icon
+                  name="wikipedia-w"
+                  type="font-awesome"
+                  size={14}
+                  underlayColor="transparent"
+                  containerStyle={globalStyles.wikiIcon}
+                  onPress={() => Linking.openURL(mission.wikipedia)}
+                />
+                <Icon
+                  name="link"
+                  type="font-awesome"
+                  size={14}
+                  underlayColor="transparent"
+                  containerStyle={globalStyles.linkIcon}
+                  onPress={() => Linking.openURL(mission.website)}
+                />
+                <Icon
+                  name="twitter"
+                  type="font-awesome"
+                  size={14}
+                  underlayColor="transparent"
+                  color={"#ffffff"}
+                  containerStyle={globalStyles.twitterIcon}
+                  onPress={() => Linking.openURL(mission.twitter)}
+                />
               </View>
             </View>
           </View>
+        </StyledView>
       </Modal>
     );
-  }
+  };
 
   render() {
     return (
       <View>
         <FlatList
-          data={ this.state.missions }
-          keyExtractor={ (item, index) => item.mission_id }
-          renderItem={ ({ item }) => this._renderItem(item) }
+          data={this.state.missions}
+          keyExtractor={(item, index) => item.mission_id}
+          renderItem={({ item }) => this._renderItem(item)}
         />
 
-        { this.state.isModalVisible ? this._renderModal(this.state.mission) : null }
+        {this.state.isModalVisible
+          ? this._renderModal(this.state.mission)
+          : null}
       </View>
     );
   }
@@ -120,6 +150,6 @@ const styles = StyleSheet.create({
   modalDescription: {
     fontSize: 14,
     marginBottom: 10,
-    justifyContent: 'center'
+    justifyContent: "center"
   }
 });
